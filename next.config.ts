7a9574f0cbaa52c8ac5ce88c563e3b9ce717274d
Angel Ronaldo Cervantes Trejo <ronaldoct.tij21@utsjr.edu.mx>
2025-07-22 22:33:30 -0600

    TEST ANGEL 3

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default {
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+}
